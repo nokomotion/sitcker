1ea650633f87eb25d24ae29dc4fb6304b832e339
Isaac Serrano <serranoie99@gmail.com>
2020-10-20 14:05:58 -0600

    Acciones de control para reproducción y ScrollView

--- components/videoReports/listReports.js
+++ components/videoReports/listReports.js
@@ -1,25 +1,48 @@
 import React from 'react'
-import { View, StyleSheet, Text, Button, SafeAreaView } from 'react-native'
+import {
+    View, 
+    StyleSheet,
+    Text, 
+    Button,
+    SafeAreaView,
+    Dimensions,
+    ScrollView } from 'react-native'
 import { Video } from 'expo-av'
 
+const {width, height} = Dimensions.get('window');
 
 const ReportsLists = ({ navigation }) => {
     return (
         <SafeAreaView>
-			<View>
-				<Video
-                    source = {{ uri: 'https://i.imgur.com/j020nsG.mp4' }}
-                    rate = {1.0}
-                    volume = {1.0}
-                    isMuted = {false}
-                    resizeMode = 'cover'
-                    shouldPlay
-                    isLooping
-                    style = {{ width: 320, height: 640 }}
-                />
-			</View>
+            <ScrollView>
+                <View>
+				    <Video
+                        source = {{ uri: 'https://i.imgur.com/j020nsG.mp4' }}
+                        rate = {1.0}
+                        volume = {1.0}
+                        isMuted = {false}
+                        resizeMode = 'cover'
+                        shouldPlay = {false}
+                        isLooping = {false}
+                        useNativeControls
+                        style = {styles.video}
+                    />
+			    </View>
+
+                <View>
+                    <Text>Reporte</Text>
+                    <Text>Grabado en: locación</Text>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    video: {
+        width: width,
+        height: height
+    }
+});
 
 export default ReportsLists
