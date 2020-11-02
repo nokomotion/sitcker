619b43fa1276ee74f77f60f26b8356a305bc9bcf
Isaac Serrano <serranoie99@gmail.com>
2020-11-02 15:19:22 -0700

    Test with list

--- components/videoReports/index.js
+++ components/videoReports/index.js
@@ -1,133 +1,52 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import {
+    View, 
+    StyleSheet,
+    Text,
+    Image,
+    SafeAreaView,
+    Dimensions,
+    ScrollView } from 'react-native'
 import { Video } from 'expo-av'
-import { FlatList, StyleSheet, SafeAreaView, Dimensions, ScrollView, SectionList, Text, View, TouchableOpacity } from 'react-native';
-import * as Permissions from 'expo-permissions';
-import * as Location from 'expo-location';
-import MapView from 'react-native-maps';
-import { ListItem } from 'react-native-elements';
 
-const response = [
-    {
-        id: '1',
-        title: 'Reporte #1'
-    },
-    {
-        id: '2',
-        title: 'Reporte #2'
-    },
-    {
-        id: '3',
-        title: 'Reporte #3'
-    },
-    {
-        id: '4',
-        title: 'Reporte #4'
-    },
-    {
-        id: '5',
-        title: 'Reporte #5'
-    },
-    {
-        id: '6',
-        title: 'Reporte #6'
-    },
-    {
-        id: '7',
-        title: 'Reporte #7'
-    },
-    {
-        id: '8',
-        title: 'Reporte #8'
-    },
-]
+const {widthVideo, heightVideo} = Dimensions.get('window');
+// Image.getSize('../.././assets/hack.png', (width, height) => {this.setState({width, height})});
 
-const ReportsLists = ({ navigation }) => {
-    const [location, setLocation] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null);
 
-    useEffect(() => {
-        (async () => {
-            let { status } = await Location.requestPermissionsAsync();
-            if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
-            }
-    
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-        })();
-    }, []);
-    
-    let text = 'Waiting...';
-
-    if (errorMsg) {
-        text = errorMsg;
-    } else if (location) {
-        text = JSON.stringify(location); 
-    }
-
+const ReportInfo = ({ navigation }) => {
     return (
         <SafeAreaView>
             <ScrollView>
-                <View style={styles.container}>
-                    <MapView style={styles.map}
-                        showUserLocation
-                        followUserLocation
-                        initialRegion={{
-                            latitude: 23.196557,
-                            longitude: -106.426483,
-                            latitudeDelta: 0,
-                            longitudeDelta: 0.005
-                        }}
+                <View>
+                    <Video
+                        source={{ uri: 'https://i.imgur.com/j020nsG.mp4' }}
+                        rate={1.0}
+                        volume={1.0}
+                        isMuted={false}
+                        resizeMode="cover"
+                        shouldPlay={false}
+                        isLooping
+                        useNativeControls
+                        style={{ width: 420, height: 680, flex: 1, zIndex: 1 }}
                     />
 
-                    <FlatList 
-                        data={ response }
-                        keyExtractor={ item => item.id }
-                        renderItem={({ item }) => {
-                        return (
-                            <ListItem chevron title={ item.title } />
-                        )
-                        }}
-                    />
-                    {/* <SectionList
-                        sections={[
-                            { title: 'D', data: ['Devin', 'Dan', 'Dominic'] },
-                            { title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie'] },
-                        ]}
-                        renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
-                        renderSectionHeader={({ section }) => (
-                            <Text style={styles.sectionHeader}>{section.title}</Text>
-                        )}
-                        keyExtractor={(item, index) => index}
-                    /> */}
-                </View>
+                    <Image style = {styles.stickerPos} source = {{uri: 'https://www.hackathones.mx/img/supporters/3.png'}}/>
+			    </View>
             </ScrollView>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1
-    },
-    sectionHeader: {
-      paddingTop: 2,
-      paddingLeft: 10,
-      paddingRight: 10,
-      paddingBottom: 2,
-      fontSize: 14,
-      fontWeight: 'bold',
-      backgroundColor: 'rgba(247,247,247,1.0)',
-    },
-    item: {
-      padding: 10,
-      fontSize: 18,
-      height: 44,
-    },
-    map: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height / 2,
+    stickerPos: {
+        margin: 5,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: 2,
+        width: 80,
+        height: 80
     }
 });
 
-export default ReportsLists
+export default ReportInfo
