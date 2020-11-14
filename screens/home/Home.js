bf6d9a41e4ad8696459ac4ad180dc78390de53c1
Isaac Serrano <serranoie99@gmail.com>
2020-11-13 20:07:56 -0700

    Added fake API and a List in the Home

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,22 +1,103 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { PureComponent } from "react";
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	TouchableOpacity,
+	Image,
+	FlatList,
+	ActivityIndicator,
+} from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function Home({ navigation }) {
-	return (
-		<View style={styles.container}>
-			<Text></Text>
-			<Button
-				title="Nos vamos a RecordReport"
-				onPress={() => navigation.navigate("RecordReport")}
+// export default function Home({ navigation }) {
+export default class Home extends PureComponent {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: [],
+		};
+	}
+
+	componentDidMount() {
+		this.getDataFromAPI();
+	}
+
+	getDataFromAPI = async () => {
+		const endPoint = "https://jsonplaceholder.typicode.com/photos?_limit=12";
+		const res = await fetch(endPoint);
+		const data = await res.json();
+		this.setState({ items: data });
+	};
+
+	_renderReport = ({ item, index }) => {
+		let { cardText, card, cardImage } = styles;
+		return (
+			<TouchableOpacity
+				style={card}
+				onPress={() => this.props.navigation.navigate("RecordInfo")}
+			>
+				<Image
+					style={cardImage}
+					source={{
+						uri: item.url,
+					}}
+				/>
+				<Text style={cardText}>Reporte: {item.title}</Text>
+			</TouchableOpacity>
+		);
+	};
+
+	render() {
+		let { container, loader } = styles;
+		let { items } = this.state;
+
+		if (items.length === 0) {
+			<View style={loader}>
+				<ActivityIndicator size="large" />
+			</View>;
+		}
+
+		return (
+			<FlatList
+				style={container}
+				data={items}
+				keyExtractor={(item, index) => index.toString()}
+				renderItem={this._renderReport}
 			/>
-		</View>
-	);
+		);
+	}
 }
 
 const styles = StyleSheet.create({
 	container: {
+		marginTop: 40,
+	},
+	cardText: {
+		fontSize: 16,
+		padding: 10,
+	},
+	card: {
+		backgroundColor: "white",
+		marginBottom: 10,
+		marginLeft: "2%",
+		width: "96%",
+		shadowColor: "#000",
+		shadowOpacity: 1,
+		shadowOffset: {
+			width: 3,
+			height: 3,
+		},
+	},
+	cardImage: {
+		width: "100%",
+		height: 200,
+		resizeMode: "cover",
+	},
+	loader: {
 		flex: 1,
-		backgroundColor: "blue",
 		alignItems: "center",
 		justifyContent: "center",
 	},
